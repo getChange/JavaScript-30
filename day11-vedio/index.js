4325ee6e20f81ce04c2e27d0d32b395e7daa08ff
hanweimin <hanweimin@15w.com>
2017-08-22 10:22:59 +0800

    add

--- day11-vedio/index.js
+++ day11-vedio/index.js
@@ -11,5 +11,21 @@ const ranges = player.querySelectorAll('.player_slider');
 //点击播放
 video.addEventListener('click', Videoplay);
 //点击或播放是改变播放和暂停状态的图标
-video.addEventListener('click', handleToggle);
+video.addEventListener('play', handleToggle);
+//暂停改变图标状态
+video.addEventListener("pause", handleToggle);
+//时间轴的改变
+video.addEventListener("timeupdate", filleUpdate);
+
+//点击播放或者暂停图标的监听
+toggle.addEventListener('click', Videoplay);
+toggle.addEventListener("click", handleToggle);
+
+//音量和播放速度滑动条事件监听
+let mouseflag = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
+
 //
